87931962c32459aa91721f8af0cc4e23da472f6d
Gabriel Godoy <107765540+Godoy-png@users.noreply.github.com>
2023-06-21 22:30:11 -0300

    Add files via upload

---
+++ MUNDO 1/ex002.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+
+print('É um prazer te conhecer,', nome+'!')
+#print('É um prazer te conhecer, {}!'.format(nome))
+#
---
+++ MUNDO 1/ex003.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite o primeiro númmero: '))
+n2 = int(input('Digite o segundo númmero: '))
+
+soma = n1 + n2
+
+print('A soma entre {} e {} vale {}!'.format(n1, n2, soma))
+
