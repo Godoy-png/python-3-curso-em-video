22ffeb6a5b721e52d82af99d789ca1c862619b8b
Gabriel Godoy <107765540+Godoy-png@users.noreply.github.com>
2023-06-28 13:41:27 -0300

    Add files via upload

---
+++ MUNDO 1/ex035.py
@@ -0,0 +1,12 @@
+print('-='*20)
+print('ANALISADOR DE TRIÂNGULOS')
+print('-='*20)
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR triângulo!')
+else:
+    print('Os segmentos acima NÃO PODEM formar triângulo!')
+
+# 31 de maio
