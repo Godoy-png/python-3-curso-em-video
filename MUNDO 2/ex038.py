8c192e6ceab8aea90d74795c02e456820d1c49a9
Gabriel Godoy <107765540+Godoy-png@users.noreply.github.com>
2023-06-28 13:43:09 -0300

    Add files via upload

---
+++ MUNDO 2/ex038.py
@@ -0,0 +1,9 @@
+n1 = int(input('Primeiro número: '))
+n2 = int(input('Segundo número: '))
+
+if n1 > n2:
+    print('O PRIMEIRO valor é o MAIOR!')
+elif n1 == n2:
+    print('Os DOIS valores são IGUAIS!')
+else:
+    print('O SEGUNDO número é o MAIOR!')
